85a370ee415576685c223b6038118dc7c23d80e7
newtbytes <186638664+Newtbytes@users.noreply.github.com>
2025-08-06 09:22:28 -0700

    Reimplement bot

--- main.go
+++ main.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/joho/godotenv"
+
+	"github.com/disgoorg/disgo"
+	"github.com/disgoorg/disgo/bot"
+	"github.com/disgoorg/disgo/discord"
+	"github.com/disgoorg/disgo/events"
+	"github.com/disgoorg/disgo/gateway"
+)
 
 func main() {
-	fmt.Println("schizoid has arisen from their slumber.")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client, err := disgo.New(os.Getenv("DISCORD_TOKEN"),
+		bot.WithGatewayConfigOpts(
+			gateway.WithIntents(
+				gateway.IntentGuildMessages,
+				gateway.IntentMessageContent,
+			),
+		),
+		bot.WithEventListenerFunc(onMessageCreate),
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer client.Close(context.TODO())
+
+	if err = client.OpenGateway(context.TODO()); err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	log.Print("schizoid is now running. Press CTRL-C to exit.")
+	s := make(chan os.Signal, 1)
+	signal.Notify(s, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-s
+}
+
+func onMessageCreate(event *events.MessageCreate) {
+	if event.Message.Author.Bot {
+		return
+	}
+	var message string
+	if event.Message.Content == "?schizoid" {
+		message = "schizoid has arisen from their slumber"
+	}
+
+	if message != "" {
+		_, _ = event.Client().Rest().CreateMessage(event.ChannelID, discord.NewMessageCreateBuilder().SetContent(message).Build())
+	}
 }
